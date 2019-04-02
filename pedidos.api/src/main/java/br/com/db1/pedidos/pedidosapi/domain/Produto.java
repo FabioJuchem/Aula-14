b0a3562084f502787dacbdaade72b7f5d88275fd
FabioJuchem <fabiojuchem@gmail.com>
2019-04-02 17:01:45 -0300

    Adicionado api

---
+++ pedidos.api/src/main/java/br/com/db1/pedidos/pedidosapi/domain/Produto.java
@@ -0,0 +1,72 @@
+package br.com.db1.pedidos.pedidosapi.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import br.com.db1.pedidos.pedidosapi.infraestrutura.Checker;
+
+@Entity
+@Table(name = "produto")
+public class Produto {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	@Column(name = "codigo",length=50, nullable = false ,unique=true)
+	private String codigo;
+	
+	@Column(name = "nome",length=100, nullable = false )
+	private String nome;
+	
+	@Column(name = "valor",precision=16,scale=2,nullable = false )
+	private Double valor;
+	
+	@Enumerated(EnumType.STRING)
+	@Column(name = "status",length=30, nullable = false)
+	private ProdutoStatus status;
+
+	
+	//construtores
+	public Produto(String codigo, String nome, Double valor) {
+		Checker.notNull(codigo, "codigo");
+		Checker.notNull(nome, "nome");
+		Checker.notNull(valor, "valor");
+		Checker.tamanhoDoCodigoDoProduto(codigo, "codigo");
+		
+		this.status = ProdutoStatus.ATIVO;
+		this.codigo = codigo;
+		this.nome = nome;
+		this.valor = valor;
+	}
+
+	public String getNome() {
+		return this.nome;
+	}
+
+	public Double getValor() {
+		return this.valor;
+	}
+
+	public String getCodigo() {
+		return this.codigo;
+	}
+	public ProdutoStatus getStatus(){
+		return this.status;
+	}
+	public void Inativar(ProdutoStatus status){
+		this.status = status;
+	}
+	public boolean produtoIsAtivo(){
+		if(this.status == ProdutoStatus.ATIVO){
+			return true;
+		}else{
+			return false;
+		}
+	}
+
+}
---
+++ pedidos.api/src/main/java/br/com/db1/pedidos/pedidosapi/infraestrutura/Checker.java
@@ -0,0 +1,42 @@
+package br.com.db1.pedidos.pedidosapi.infraestrutura;
+
+import java.util.List;
+
+public class Checker {
+	
+	public static void notNull(Object value, String field){
+		if(value == null){
+			throw new RuntimeException("Campo " + field + " � obrigat�rio.");
+		}
+	}
+	
+	public static void valorMenorQueZero(Double value, String field){
+		if(value <= 0 ){
+			throw new RuntimeException("Valor Invalido");
+		}
+	}
+	public static void tamanhoCpfIncorreto(String value, String field){
+		if(value.length() < 11 ){
+			throw new RuntimeException("CPF Invalido");
+		}
+	}
+	public static void tamanhoDoCodigoDoProduto(String value, String field) {
+		if(value.length() < 5 && value.length() > 5) {
+			throw new RuntimeException("C�digo com tamanho invalido");
+		}
+	}
+	public static void quantidadeMaximaDeItens(Double value, String field) {
+		if(value > 10) {
+			throw new RuntimeException("Quantidade de itens excedida");
+		}
+	}
+	public static void quantidadeMaximadeProdutos(List<PedidoItem> produtos,String field) {
+		if(produtos.size() > 10) {
+			throw new RuntimeException("Quantidade de produtos excedida");
+		}
+	}
+	
+
+	
+
+}
